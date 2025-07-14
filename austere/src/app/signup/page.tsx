88d8fa4ed285d1c3e4e1ef2ce8bc34173b001eb0
SenseiChromosome <ianspoonerxxx@icloud.com>
2025-07-14 11:28:59 +0100

    added basic navigation

---
+++ austere/src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Signup() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-white">
+      <h1 className="text-black text-4xl mb-8">Signup Page</h1>
+      <Link href="/">
+        <button className="px-4 py-2 border border-black text-black rounded">Back to Home</button>
+      </Link>
+    </div>
+  );
+} 
