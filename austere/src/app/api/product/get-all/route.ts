0fe383fe2ed88b274219757b4526f75106a8023c
DylanBk <d.bullock1404@outlook.com>
2025-07-14 17:57:09 +0100

    updated environment variable names + setup supabase

--- austere/src/app/api/product/get-all/route.ts
+++ austere/src/app/api/product/get-all/route.ts
@@ -7,7 +7,7 @@ import { s3Client as client } from "@/config/config";
 export async function GET() {
     try {
         const input = {
-            Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME as string,
+            Bucket: process.env.AWS_S3_BUCKET_NAME as string,
         };
         const command = new ListObjectsV2Command(input);
         const res = await client.send(command);
--- austere/src/config/config.ts
+++ austere/src/config/config.ts
@@ -1,9 +1,14 @@
+import { Pool } from "pg";
 import { S3Client } from "@aws-sdk/client-s3";
 
+export const db = new Pool({
+    connectionString: process.env.SUPABASE_URI
+});
+
 export const s3Client = new S3Client ({
-    region: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_REGION as string,
+    region: process.env.AWS_S3_BUCKET_REGION as string,
     credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_USER_ACCESS_KEY as string,
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_USER_SECRET_ACCESS_KEY as string
+        accessKeyId: process.env.AWS_USER_ACCESS_KEY as string,
+        secretAccessKey: process.env.AWS_USER_SECRET_ACCESS_KEY as string
     }
 });
