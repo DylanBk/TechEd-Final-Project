7e270c2e9a07c33ba5e653c80efc39e35019b68e
DylanBk <d.bullock1404@outlook.com>
2025-07-16 13:03:41 +0100

    updated schema

--- austere/src/lib/schema.ts
+++ austere/src/lib/schema.ts
@@ -1,13 +1,40 @@
 import * as z from 'zod';
 
+export const SignupSchema = z.object({
+    name: z
+        .string()
+        .min(2)
+        .max(50)
+        .trim(),
+    email: z
+        .email(),
+    password: z
+        .string()
+        .min(8)
+        .max(32)
+        .trim()
+});
+
 export const ProductSchema = z.object({
-    id: z.string(),
-    category: z.string(),
-    name: z.string(),
-    description: z.string(),
-    price: z.string(),
+    id: z
+        .string()
+        .trim(),
+    category: z
+        .string()
+        .trim(),
+    name: z
+        .string()
+        .min(1)
+        .trim(),
+    description: z
+        .string()
+        .min(1)
+        .trim(),
+    price: z
+        .string()
+        .trim(),
     image: z.object({
-        src: z.string(),
-        alt: z.string()
+        src: z.string().trim(),
+        alt: z.string().trim()
     })
 });
